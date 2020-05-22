62fccc056145bcf2012b6698f813d887d0304e0c
N-DucharmeBarth <nicholasd@spc.int>
2020-05-22 11:05:48 +1100

    update plot.comparisons.r to handle the palette argument that Finlay made

--- R/plot.comparisons.r
+++ R/plot.comparisons.r
@@ -6,13 +6,14 @@
 #' @param agg.regions TRUE or FALSE. Should model outputs be aggregated across all regions are kept separate.
 #' @param sel.basis A character string indicating if selectivity at age ('AGE') or length ('Length') should be plotted
 #' @param biomass.type Character string denoting the type of biomass plotted, 'SSB' or 'Total'
-#' @param palette.cols A vector of character strings giving the colors to form a palette for differentiating between models. If wishing to use the exact colors give a vector of (length(rep.list) - 1) colors as the reference model is black by default.
+#' @param palette.func A function to determine the colours of the models. The default palette has the reference model in black. It is possible to determine your own palette function. Two functions currently exist: default.model.colours() and colourblind.model.colours().
+#' @param ... Passes extra arguments to the palette function. Use the argument all.model.colours to ensure consistency of model colours when plotting a subset of models.
 #' @param save.dir Path to the directory where the outputs will be saved
 #' @param save.name Name stem for the output, useful when saving many model outputs in the same directory
 #' @export
 #' 
 
-plot.comparisons = function(plot.name,rep.list,rep.names,agg.years = TRUE,agg.regions=TRUE,sel.basis="AGE",biomass.type = "SSB", palette.cols = c("royalblue3","deepskyblue1","gold","orange1","indianred1","firebrick2","#AC2020"),save.dir,save.name)
+plot.comparisons = function(plot.name,rep.list,rep.names,agg.years = TRUE,agg.regions=TRUE,sel.basis="AGE",biomass.type = "SSB",palette.func=default.model.colours,save.dir,save.name,...)
 {
 	if(!missing(save.dir))
 	{
@@ -21,9 +22,9 @@ plot.comparisons = function(plot.name,rep.list,rep.names,agg.years = TRUE,agg.re
 			stop("How can you save the output if you haven't specified the name? Please specify 'save.name'.")
 		} else {
 			if (! dir.exists(save.dir))dir.create(save.dir,recursive=TRUE)
-			g1 = plot.depletion(rep.list,rep.names,agg.years,agg.regions,biomass.type,palette.cols,save.dir,save.name=paste0("dep-",save.name))
-			g2 = plot.biomass(rep.list,rep.names,agg.years,agg.regions,biomass.type,palette.cols,save.dir,save.name=paste0("bio-",save.name))
-			g3 = plot.selectivity(rep.list,rep.names,sel.basis,palette.cols,save.dir,save.name=paste0("sel-",tolower(sel.basis),"-",save.name))
+			g1 = plot.depletion(rep.list,rep.names,agg.years,agg.regions,biomass.type,palette.func,save.dir,save.name=paste0("dep-",save.name),...)
+			g2 = plot.biomass(rep.list,rep.names,agg.years,agg.regions,biomass.type,palette.func,save.dir,save.name=paste0("bio-",save.name),...)
+			g3 = plot.selectivity(rep.list,rep.names,sel.basis,palette.func,save.dir,save.name=paste0("sel-",tolower(sel.basis),"-",save.name),...)
 		}
 	} else {
 		stop("Must provide 'save.dir'.")
