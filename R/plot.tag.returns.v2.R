23d52900a18846f81de92cbdc7021b56ff80320a
Finlay Scott <drfinlayscott@gmail.com>
2020-07-09 11:06:55 +1100

    Added variant of tag returns plot that takes different input data to save space

---
+++ R/plot.tag.returns.v2.R
@@ -0,0 +1,234 @@
+# Tagging plots
+
+#' Plot the observed and predicted tag recaptures.
+#' 
+#' Plot the observed and predicted tag recaptures by tag recapture group.
+#' Tags caught during the mixing period are not counted.
+#' The plot is either a time series of the difference between the observed and predicted, or a time series of the recaptures.
+#' A loess smoother is put through the differences.
+#' @param tagrelease.list A list, or an individual data.frame, of tag release data created by the \code{tag.release.data.preparation()} function.
+#' @param tagrep.list A list, or an individual data.frame, of tag returns from the temporary_tag_returns file. The output from the FLR4MFCL function \code{read.temporary_tag_report()}.
+#' @param fishery.map A data.frame that describes which fishery is fishing in which region and with which gear. The columns are: fishery_name, region, gear, fishery (the reference number), tag_recapture_group and tag_recapture_name. The map should contain entries for all fisheries to avoid data being inadvertently dropped.
+#' @param names A vector of character strings naming the models for plotting purposes. If not supplied, model names will be taken from the names in the tagrelease.list (if available) or generated automatically.
+#' @param recapture.groups A vector of the reference numbers of the tag recapture groups you want to plot.
+#' @param plot.diff Do you want to plot the difference between the observed and predicted, or a time series of recaptures? TRUE (default) or FALSE.
+#' @param scale.diff If TRUE, the difference between observed and predicted is scaled by the mean of observed returns.
+#' @param show.legend Do you want to show the plot legend, TRUE (default) or FALSE.
+#' @param show.points Do you want to show points as well as the smoother for the difference plots? Default is FALSE.
+#' @param palette.func A function to determine the colours of the models. The default palette has the reference model in black. It is possible to determine your own palette function. Two functions currently exist: default.model.colours() and colourblind.model.colours().
+#' @param save.dir Path to the directory where the outputs will be saved
+#' @param save.name Name stem for the output, useful when saving many model outputs in the same directory
+#' @param ... Passes extra arguments to the palette function. Use the argument all.model.names to ensure consistency of model colours when plotting a subset of models.
+#' @export
+#' @import FLR4MFCL
+#' @import magrittr
+#' @importFrom data.table data.table
+#' @importFrom data.table rbindlist
+#' @importFrom ggthemes theme_few
+#' @importFrom ggplot2 geom_blank
+# 
+# 
+# Without tagdat.list but with individual elements
+plot.tag.returns.time.v2 <- function(tagrelease.list, tagrep.list, fishery.map, names=NULL, recapture.groups, plot.diff=TRUE, scale.diff=TRUE, show.legend=TRUE, show.points=FALSE, palette.func=default.model.colours, save.dir, save.name, ...){
+  
+  # If not plotting the difference don't scale it
+  if(plot.diff == FALSE){
+    scale.diff <- FALSE
+  }
+  
+  tagrelease.list <- check.tagrelease.args(tagrelease.list, names) 
+  tagrep.list <- check.tagrep.args(tagrep.list, names) 
+  
+  # If plotting time series of actuals, can only plot one model at a time
+  if(plot.diff == FALSE & length(tagrelease.list) != 1){
+    stop("If plotting actual observed and predicted attrition of (not the difference between them) you can only plot one model at a time. Try subsetting your tagrelease list.")
+  }
+  
+  # Collapse recaptures into a single data.table
+  tagrep <- data.table::rbindlist(tagrep.list, idcol="Model")
+  # Bring in tag_recapture_group and name
+  colnames(fishery.map)[colnames(fishery.map)=="fishery"] <- "recap.fishery"
+  tagrep <- merge(tagrep, fishery.map[,c("recap.fishery", "tag_recapture_group", "tag_recapture_name")])
+  # Drop recapture groups we don't want
+  tagrep <- tagrep[tag_recapture_group %in% recapture.groups,]
+  # Make a timestep column
+  tagrep$recap.ts <- tagrep$recap.year + (tagrep$recap.month-1)/12 + 1/24
+  # Drop columns we don't need for space
+  tagrep[,c("recap.year", "recap.month", "recap.fishery"):=NULL]
+  
+  # Get releases
+  tagrel <- data.table::rbindlist(tagrelease.list, idcol="Model")
+  tagrel$rel.ts.after.mix <- tagrel$rel.ts + tagrel$mixing_period_years
+  # Bring mixing period into recapture
+  tagrep <- merge(tagrep, tagrel[,c("Model", "rel.group", "rel.ts.after.mix")])
+  # Drop observations that are within the mixing period  
+  tagrep <- tagrep[!(recap.ts < rel.ts.after.mix),]
+  
+  # Summarise returns by recapture group
+  pdat <- tagrep[,.(recap.pred = sum(recap.pred, na.rm=TRUE), recap.obs = sum(recap.obs, na.rm=TRUE)), by=.(tag_recapture_group, tag_recapture_name, recap.ts, Model)]  
+  
+  # To ensure plotting is OK we need each fishery to have a full complement of time series
+  # observations, even if NA.
+  # This is a right pain in the bum - must be an easier way
+  # Need to pad out time series
+  no_seasons <- length(unique(tagrep.list[[1]]$recap.month)) # this is unsafe, how best to get no seasons?
+  padts <- expand.grid(recap.ts = seq(from=min(pdat$recap.ts), to=max(pdat$recap.ts), by= 1 / no_seasons), tag_recapture_name = sort(unique(pdat$tag_recapture_name)), Model=sort(unique(pdat$Model)))
+  # Bring in the recapture name
+  # Careful here as recapture_group gets filled with NAs
+  pdat <- merge(pdat, padts, all=TRUE, by=colnames(padts))
+  
+  # Want pdat to have Model names in the original order - important for plotting order
+  pdat[,Model:=factor(Model, levels=names(tagrep.list))]
+  
+  # Mathew's plot. Time series of predicted and observed
+  if(plot.diff == FALSE){
+    # For the observed and predicted recaptures, NA is essentially 0,
+    # i.e. there were no recaptures, so set to 0
+    pdat[is.na(pdat$recap.pred), "recap.pred"] <- 0
+    pdat[is.na(pdat$recap.obs), "recap.obs"] <- 0
+    p <- ggplot2::ggplot(pdat, ggplot2::aes(x=recap.ts, y=recap.obs))
+    p <- p + ggplot2::geom_point(colour="red", na.rm=TRUE)
+    p <- p + ggplot2::geom_line(ggplot2::aes(y=recap.pred), na.rm=TRUE)
+    p <- p + ggplot2::facet_wrap(~tag_recapture_name, scales="free")
+    p <- p + ggplot2::xlab("Time") + ggplot2::ylab("Tag recaptures")
+    p <- p + ggthemes::theme_few()
+  }
+  
+  
+  if(plot.diff == TRUE){
+    colour_values <- palette.func(selected.model.names = names(tagrelease.list), ...)
+    # Or plot the difference - need to scale by number of recaptures?
+    pdat$diff <- pdat$recap.obs - pdat$recap.pred
+    ylab <- "Observed - predicted recaptures"
+    # Normalise
+    if(scale.diff == TRUE){
+      # Rescale the diffs by the mean recaptures in that group
+      pdat <- pdat[,.(Model=Model, recap.ts=recap.ts, diff = diff / mean(recap.obs, na.rm=TRUE)), by=.(tag_recapture_name)]
+      ylab <- "Obs. - pred. recaptures (scaled)"
+    }
+    # Spoof up approriate y ranges for each facet using geom_blank()
+    dummydat <- pdat[,.(y = c(max(abs(diff), na.rm=T), -max(abs(diff), na.rm=T))), by=.(tag_recapture_name)]
+    dummydat$x <- rep(c(min(pdat$recap.ts), max(pdat$recap.ts)), nrow(dummydat)/2)
+    
+    p <- ggplot2::ggplot(pdat, ggplot2::aes(x=recap.ts, y=diff))
+    # If showing points, also make y axes symmetrical
+    # If you want symetrical points with just the smoother look at
+    # https://stackoverflow.com/questions/9789871/method-to-extract-stat-smooth-line-fit 
+    if(show.points==TRUE){
+      p <- p + ggplot2::geom_point(aes(colour=Model), na.rm=TRUE)
+      p <- p + ggplot2::geom_blank(data=dummydat, aes(x=x, y=y))
+    }
+    p <- p + ggplot2::geom_smooth(aes(colour=Model), method = 'loess', formula = 'y~x', na.rm=TRUE, se=FALSE)
+    p <- p + ggplot2::scale_color_manual("Model",values=colour_values)
+    p <- p + ggplot2::facet_wrap(~tag_recapture_name, scales="free")
+    p <- p + ggplot2::geom_hline(ggplot2::aes(yintercept=0.0), linetype=2)
+    p <- p + ggthemes::theme_few()
+    p <- p + ggplot2::xlab("Time") + ggplot2::ylab(ylab)
+    if (show.legend==FALSE){
+      p <- p + theme(legend.position="none") 
+    }
+  }
+  
+  save_plot(save.dir, save.name, plot=p)
+  
+  return(p)
+}
+
+
+
+
+
+##' Plot proportions of tag returns by region
+##' 
+##' Plot the difference between the predicted and observed proportions of tag returns by region.
+##' Experimental at the moment.
+##' @param tagdat A data.frame of tagging data created by the \code{tag.data.preparation()} function.
+##' @param plot.type What type of plot: 'point' (default) or 'bar'. 
+##' @param save.dir Path to the directory where the outputs will be saved.
+##' @param save.name Name stem for the output, useful when saving many model outputs in the same directory.
+##' @export
+##' @import FLR4MFCL
+##' @import magrittr
+#plot.tag.return.proportion <- function(tagdat, plot.type="point", save.dir, save.name){
+#  
+#  # Could use data.table here
+#  # Get sum of recaptures by release and recapture region and by recapture quarter (month)
+#  recap_reg <- aggregate(list(recap.pred=tagdat$recap.pred, recap.obs=tagdat$recap.obs),
+#    list(rel.region = tagdat$rel.region, recap.region=tagdat$recap.region, recap.month=tagdat$recap.month),
+#    sum, na.rm=TRUE)
+#
+#  # Get total recaptures by release region and month
+#  recap_reg_sum <- aggregate(list(recap.pred.sum = tagdat$recap.pred, recap.obs.sum = tagdat$recap.obs),
+#    list(rel.region = tagdat$rel.region, recap.month = tagdat$recap.month),
+#    sum, na.rm=TRUE)
+#
+#  # Merge together and get the proportion of recaptures 
+#  # i.e. the total number of tags from region 1 that were recaptured, found out the proportion that was recaptured in each region
+#  recap_reg <- merge(recap_reg, recap_reg_sum)
+#  recap_reg$pred.prop <- recap_reg$recap.pred / recap_reg$recap.pred.sum
+#  recap_reg$obs.prop <- recap_reg$recap.obs/ recap_reg$recap.obs.sum
+#
+#  # Sanity check that it looks like data in Fig 33 of SKJ 2019 assessment report
+#  # Total released from 1 that were recaught in 1 quarter
+#  #subset(recap_reg_sum, recap.month==2 & rel.region==1)
+#  #subset(recap_reg, recap.month==2 & rel.region==1)
+#
+#  # Bare bones of Fig 33 of SKJ 2019 assessment report
+#  # Not continued as we should try something else
+#  #p <- ggplot(recap_reg, aes(x=rel.region, y=recap.region))
+#  #p <- p + geom_raster(aes(fill=obs.prop))
+#  #p <- p + facet_wrap(~recap.month)
+#  #p
+#
+#  # Plot of the difference between predicted and observed proportion of tags returned by region of release
+#  recap_reg$diff.prop <- recap_reg$obs.prop - recap_reg$pred.prop
+#  #recap_reg$diff.prop2 <- log(recap_reg$pred.prop / recap_reg$obs.prop)
+#  recap_reg$rel.region.name <- paste("Release region ", recap_reg$rel.region, sep="")
+#  recap_reg$Quarter <- as.factor((recap_reg$recap.month+1) / 3)
+#
+#  # Plot attempt 1 - Facet for each release region, plot difference in proportion
+#  # by recapture region, coloured by quarter
+#  
+#  # Stuff for dummy data
+#  no_grps <- length(unique(recap_reg$rel.region.name))
+#  ylims <- tapply(recap_reg$diff.prop, recap_reg$rel.region.name, function(x) c(max(abs(x), na.rm=T), -max(abs(x), na.rm=T)))
+#  dummydat <- data.frame(y=unlist(ylims), x=rep(c(min(recap_reg$recap.region), max(recap_reg$recap.region)), no_grps), rel.region.name = rep(names(ylims), each=2))
+#  dummydat$y <- dummydat$y * 1.1
+#  
+#  # Point plot
+#  if(plot.type == "point"){
+#    p <- ggplot2::ggplot(recap_reg, ggplot2::aes(x=as.factor(recap.region), y=diff.prop))
+#    p <- p + ggplot2::geom_point(aes(colour = Quarter), size=4)
+#    p <- p + ggplot2::facet_wrap(~rel.region.name, ncol=2, scales="free")
+#    p <- p + ggplot2::geom_hline(ggplot2::aes(yintercept=0.0), linetype=2)
+#    p <- p + ggthemes::theme_few()
+#    p <- p + ggplot2::xlab("Recapture region")
+#    p <- p + ggplot2::ylab("Observed proportion - predicted proportion")
+#    p <- p + ggplot2::geom_blank(data=dummydat, ggplot2::aes(x=x, y=y))
+#  }
+#
+#  # Bar plot
+#  if(plot.type == "bar"){
+#    p <- ggplot2::ggplot(recap_reg, ggplot2::aes(x=as.factor(recap.region), y=diff.prop))
+#    p <- p + ggplot2::geom_bar(stat="identity", ggplot2::aes(fill = Quarter), colour="black", position=ggplot2::position_dodge())
+#    p <- p + ggplot2::facet_wrap(~rel.region.name, ncol=2, scales="free")
+#    p <- p + ggplot2::xlab("Recapture region")
+#    p <- p + ggplot2::geom_hline(aes(yintercept=0.0))
+#    p <- p + ggplot2::ylab("Observed proportion - predicted proportion")
+#    p <- p + ggthemes::theme_few()
+#    p <- p + ggplot2::geom_blank(data=dummydat, ggplot2::aes(x=x, y=y))
+#  }
+#  
+#  save_plot(save.dir, save.name, plot=p)
+#  
+#  return(p)
+#}
+#
+#
+#
+#
+#
+#
+#
+#
+#
--- R/tag.data.preparation.R
+++ R/tag.data.preparation.R
@@ -1,7 +1,44 @@
 # Tag data preparation for plotting
 # The tag plots need to combine data from different sources.
-# It is easier to do that before we call the function, rather than doing it in the 
-# function.
+# It is easier to do that before we call the function, rather than doing it in the plot function.
+
+
+
+#' Prepare tag release data for tagging plots
+#' 
+#' The data required for the tagging plots are in different sources (temporary_tag_report, tag files, par files).
+#' This function pulls the data together the release data from the tag file and the par file (for the mixing period).
+#' This can be passed, along with the recapture data, into the tag plot functions
+#' @param tagobs An object of type MFCLTag.
+#' @param par An object of MFCLPar that contains the effort deviations.
+#' @export
+#' @import FLR4MFCL
+tag.release.data.preparation <- function(tagobs, par){
+  if (class(par) != "MFCLPar"){
+    stop("par argument must of type of type 'MFCLPar'.")
+  }
+  if (class(tagobs) != "MFCLTag"){
+    stop("obs argument must of type of type 'MFCLTag'.")
+  }
+  
+  # Get total release numbers by release event
+  tag_releases <- aggregate(list(rel.obs = releases(tagobs)$lendist),
+    list(program=releases(tagobs)$program, rel.group=releases(tagobs)$rel.group, rel.region=releases(tagobs)$region, rel.year=releases(tagobs)$year, rel.month=releases(tagobs)$month),
+  sum, na.rm=TRUE)
+  # Bring in the mixing period
+  tag_releases$mixing_period <- flagval(par, (-10000 - tag_releases$rel.group + 1),1)$value
+  # What is the mixing period in terms of years?
+  no_seasons <- dimensions(par)["seasons"]
+  tag_releases$mixing_period_years <- tag_releases$mixing_period / no_seasons
+  tag_releases$rel.ts <- tag_releases$rel.year + (tag_releases$rel.month-1)/12 + 1/24
+  
+  # Reorder for friendliness back in calling function
+  tag_releases <- tag_releases[order(tag_releases$rel.group, tag_releases$rel.ts),]
+  
+  return(tag_releases)
+}
+
+
 
 #' Prepare data for tagging plots
 #' 
